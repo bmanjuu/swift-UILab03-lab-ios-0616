fb1064ed9e89df898266700eab4eea8514568a15
bmanjuu <bettyofung@gmail.com>
2016-07-13 09:36:46 -0400

    donee

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -11,9 +11,45 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Create IB outlets
 
+    @IBOutlet weak var middleNumberLabel: UILabel!
+    @IBOutlet weak var cornerSuit: UILabel!
+    @IBOutlet weak var otherCornerSuit: UILabel!
+    //can only connect one IBOutlet to one thing!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // TODO: IB actions and code to update UI
+    
+    @IBAction func cardButtons(sender: UIButton) {
+        // sender.currentTitle is an optional!
+        // need to be more explicit with what the sender is! originally, it was anyObject (so sender.currentTitle was of type String?! but change it to UIButton. so the "!" meant that I needed to be more explicit with who the sender is)
+        if let card = sender.currentTitle {
+            switch card {
+                case "♣️4":
+                    middleNumberLabel.text = "4"
+                    cornerSuit.text = "♣️"
+                    otherCornerSuit.text = "♣️"
+                case "♠️3":
+                    middleNumberLabel.text = "3"
+                    cornerSuit.text = "♠️"
+                    otherCornerSuit.text = "♠️"
+                case "♦️8":
+                    middleNumberLabel.text = "8"
+                    cornerSuit.text = "♦️"
+                    otherCornerSuit.text = "♦️"
+                case "♥️10":
+                    middleNumberLabel.text = "10"
+                    cornerSuit.text = "♥️"
+                    otherCornerSuit.text = "♥️"
+                default:
+                    middleNumberLabel.text = "?"
+                    cornerSuit.text = "?"
+                    otherCornerSuit.text = "?"
+            } // end of switch
+        } // end of if statement
+    } // end of IBAction
+
+//THE END
 }
